d2f79a4f7a9703f96561be0646fbb1b72ecd3cb0
Linus Oleander <linus@oleander.nu>
2011-05-08 15:04:39 +0200

    Adding a better cache

--- lib/btjunkie/torrent.rb
+++ lib/btjunkie/torrent.rb
@@ -4,7 +4,7 @@ module BtjunkieContainer
   class Torrent
     attr_accessor :title, :torrent    
     def initialize(args)
-      @subtitle = {}
+      @_subtitle = {}
       args.keys.each { |name| instance_variable_set "@" + name.to_s, args[name] }
     end
     
@@ -21,7 +21,7 @@ module BtjunkieContainer
     
     # Generates an id using the details url
     def id
-      @id ||= torrent.match(/(\w+)\/download\.torrent$/)[1]
+      @_id ||= torrent.match(/(\w+)\/download\.torrent$/)[1]
     end
     
     # Returns the domain for the torrent, without http or www
@@ -32,12 +32,14 @@ module BtjunkieContainer
     
     # Returns a unique id for the torrent based on the domain and the id of the torrent
     def tid
-      @tid ||= Digest::MD5.hexdigest("#{domain}#{id}")
+      @_tid ||= Digest::MD5.hexdigest("#{domain}#{id}")
+    rescue => error
+      raise "Details: #{details} #{error}"
     end
     
     # Just a mirror method for {tid}, just in case someone don't like the method name tid
     def torrent_id
-      @torrent_id ||= tid
+      @_torrent_id ||= tid
     end
     
     # Returns an movie_searcher object based on the imdb_id, if it exists, otherwise the torrent title
@@ -53,11 +55,11 @@ module BtjunkieContainer
     # Read more about it here: https://github.com/oleander/Undertexter
     # Return type: A single Undertexter object or nil
     def subtitle(option = :english)
-      @subtitle[option] ||= Undertexter.find(movie.imdb_id, language: option).based_on(title)
+      @_subtitle[option] ||= Undertexter.find(movie.imdb_id, language: option).based_on(title)
     end
     
     def details
-      "http://#{domain}#{@details}"
+      @_details ||= "http://#{domain}#{@details}"
     end
     
     def imdb_id; nil; end
